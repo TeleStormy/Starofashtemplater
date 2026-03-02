0e074cbeb55ed84583a9dd1df8172509d51b4154
TeleStormy <soupuhmangaming@gmail.com>
2026-03-02 03:32:25 -0800

    Update csv.js

--- js/csv.js
+++ js/csv.js
@@ -1,1 +1,34 @@
+let cards = [];
 
+document.getElementById("csvUpload").addEventListener("change", function(e) {
+  const file = e.target.files[0];
+
+  Papa.parse(file, {
+    header: true,
+    complete: function(results) {
+      cards = results.data.filter(c => c.Name);
+      buildCardList();
+      renderCard(cards[0]);
+    }
+  });
+});
+
+function buildCardList() {
+  const list = document.getElementById("cardList");
+  list.innerHTML = "";
+
+  cards.forEach((card, index) => {
+    const div = document.createElement("div");
+    div.className = "card-item";
+    div.innerText = card.Name;
+
+    div.addEventListener("click", () => {
+      document.querySelectorAll(".card-item")
+        .forEach(i => i.classList.remove("active"));
+      div.classList.add("active");
+      renderCard(card);
+    });
+
+    list.appendChild(div);
+  });
+}
